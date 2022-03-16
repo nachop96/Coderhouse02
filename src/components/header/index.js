37410ca1b9994e8fe409553cf7bddd16b78cbd94
nachop96 <ignapedrosa@gmail.com>
2022-03-16 15:39:03 -0300

    Agregar src, componentes y header

---
+++ src/components/header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+import App from '../../App';
+
+import { styles } from './styles';
+
+
+
+
+const Header = ( {title }) => {
+
+
+    
+    return (
+
+        
+
+        <View  style={styles.container}>
+            <Text style = {styles.title}> {title} </Text>
+
+        </View>
+
+    );
+
+};
+
+export default Header;
---
+++ src/components/header/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+
+    container: {
+        flex:1,
+        flexGrow: 0.18,
+        justifyContent: 'center',
+        backgroundColor:'#335145',
+        alignItems: 'center',
+        
+       
+    },
+
+    title:{
+        fontSize:20,
+        fontWeight:'bold',
+        textAlign:'center',
+        margin: 10,
+        color:'#BEEF9E',
+
+
+    },
+
+    
+
+
+});
